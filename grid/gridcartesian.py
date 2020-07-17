c396a5588f346973edd8bbf91d77e69dbfa233e6
TheBandik <shnr.arky@gmail.com>
2020-07-17 14:27:25 +1000

    Масштабирование сетки

--- grid/gridcartesian.py
+++ grid/gridcartesian.py
@@ -1,5 +1,6 @@
 from .grid import Grid
 from PySide2.QtGui import QPen, Qt
+from PySide2.QtCore import QPoint
 
 
 class GridCartesian(Grid):
@@ -9,13 +10,31 @@ class GridCartesian(Grid):
         self.painter = painter
         self.width = self.graphwidget.width()
         self.height = self.graphwidget.height()
+        self.origin = QPoint(self.graphwidget.width() / 2,
+                             self.graphwidget.height() / 2)
         self.grid()
         self.axis()
 
     def grid(self):
         pen = QPen(Qt.gray, 1, Qt.DotLine)
         self.painter.setPen(pen)
-        for i in range(20, self.width, 20):
+        # 1 четверть
+        for i in range(self.origin.x(), self.width, 20):
+            self.painter.drawLine(i, 0, i, self.height / 2)
+        for i in range(self.origin.y(), 0, -20):
+            self.painter.drawLine(self.width / 2, i, self.width, i)
+        # 2 четверть
+        for i in range(self.origin.x(), 0, -20):
+            self.painter.drawLine(i, 0, i, self.height / 2)
+        for i in range(self.origin.y(), 0, -20):
+            self.painter.drawLine(self.width / 2, i, 0, i)
+        # 3 четверть
+        for i in range(self.origin.x(), 0, -20):
+            self.painter.drawLine(i, self.height / 2, i, self.height)
+        for i in range(self.origin.y(), self.height, 20):
+            self.painter.drawLine(self.width / 2, i, 0, i)
+        # 4 четверть
+        for i in range(self.origin.x(), self.width, 20):
             self.painter.drawLine(i, 0, i, self.height)
-        for i in range(20, self.height, 20):
-            self.painter.drawLine(0, i, self.width, i)
+        for i in range(self.origin.y(), self.height, 20):
+            self.painter.drawLine(self.width / 2, i, self.width, i)
