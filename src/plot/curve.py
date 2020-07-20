e387333c580dd074466fd6584008f90e208df0f5
TheBandik <shnr.arky@gmail.com>
2020-07-20 22:30:18 +1000

    Редактура файла curve.py

--- src/plot/curve.py
+++ src/plot/curve.py
@@ -4,36 +4,53 @@ from PySide2.QtCore import QPointF, Qt
 from PySide2.QtGui import QPen
 
 
+# Отрисовка кривой
 class Curve():
 
-    def __init__(self, graphwidget, painter, a):
-        self.graphwidget = graphwidget
+    def __init__(self, graphWidget, painter, a):
+        # Определение полученных переменных
+        self.graphWidget = graphWidget
         self.painter = painter
         self.a = a * 10
+        # Последняя полученная точка
         self.lastCoord = QPointF()
+        # Первая точка
         self.first = True
-        self.origin = QPointF(self.graphwidget.width() / 2,
-                              self.graphwidget.height() / 2)
+        # Центр координат
+        self.origin = QPointF(self.graphWidget.width() / 2,
+                              self.graphWidget.height() / 2)
+        # Рисование кривой
         self.draw()
 
+    # Рисование кривой
     def draw(self):
         pen = QPen(Qt.blue, 1, Qt.SolidLine)
         self.painter.setPen(pen)
-        fi = floor(self.graphwidget.height() / -35.8 / (self.a / 10))
-        end = ceil(self.graphwidget.height() / 35.8 / (self.a / 10))
+        # Начальная точка
+        fi = floor(self.graphWidget.height() / -35.8 / (self.a / 10))
+        # Конечная точка
+        end = ceil(self.graphWidget.height() / 35.8 / (self.a / 10))
+        # Отрисовка всех точек
         while fi <= end:
+            # Нахождение точки
             point = QPointF()
             point = self.findPoint(fi)
+            # Если точка первая, то она не рисуется
             if self.first:
                 self.first = False
             else:
                 self.painter.drawLine(point, self.lastCoord)
+            # Запись прошлой точки
             self.lastCoord = point
+            # Следующая итерация
             fi += 0.01
 
+    # Нахождение точки
     def findPoint(self, t):
         coord = QPointF()
+        # Параметрические формулы кривой
         coord.setX((2 * self.a * t ** 2) / (1 + t ** 2))
         coord.setY((2 * self.a * t ** 3) / (1 + t ** 2))
+        # Смещение относительно центра координат
         coord += self.origin
         return coord
